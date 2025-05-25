efd34986b459c24e4741e9d925d08b6b13d287f6
jethro34 <jh.triana@hotmail.com>
2025-05-24 20:30:00 -0400

    add file to read rockyou.txt and create file with rainbow table

---
+++ attack-demos/password_hash_extraction/create_rainbow_table.py
@@ -0,0 +1,33 @@
+import hashlib
+import json
+import os
+
+rainbow_table_filepath = "rainbow_table.json"
+
+# check if rainbow table exists
+if os.path.exists(rainbow_table_filepath):
+    print("Rainbow table exists.")
+else:
+    print("Rainbow table does not exist. Creating it...")
+
+    rockyou_filepath = "rocku_1st1k.txt"    # use "rockyou.txt" for full list
+
+    # read & clean passwords
+    with open(rockyou_filepath, "r", errors="ignore") as pwd_file:
+        rockyou_pwds_raw = pwd_file.readlines()
+
+    # generate hashes & create rainbow table
+    md5_hash_table = {}     # dictionary to store hashes
+    for raw_pwd in rockyou_pwds_raw:
+        pwd_clean = raw_pwd.rstrip()    # remove trailing newline character
+        pwd_bytes = pwd_clean.encode('utf-8')   # convert password to array of bytes
+        md5_hash = hashlib.md5(pwd_bytes).hexdigest()   # generate hash
+        md5_hash_table[md5_hash] = pwd_clean    # store as {hash: password}
+
+    # create & write contents to rainbow table as JSON file
+    try:
+        with open(rainbow_table_filepath, "w") as json_file:
+            json.dump(md5_hash_table, json_file)
+        print("Rainbow table created.")
+    except Exception as e:
+        print(f"Error creating the rainbow table:", e)
